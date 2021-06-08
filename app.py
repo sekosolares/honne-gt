a7c68e761e76d2ac4d6bc7e7d93e6bf9e7b8f1ac
sekosolares <axel.solares.g@gmail.com>
2021-06-08 01:44:49 -0600

    Edicion de rutas y templates

--- app.py
+++ app.py
@@ -15,5 +15,33 @@ def como_leer_manga():
 def mangas():
     return render_template('Mangas.html')
 
+@app.route('/webtoons')
+def webtoons():
+    return render_template('WebToons.html')
+
+@app.route('/explorar')
+def explorar():
+    return render_template('Explorar.html')
+
+@app.route('/contenido/<template_name>')
+def go_to(template_name):
+    if template_name == 'KimetsuNoYaiba':
+        return render_template('KimetsuNoYaiba.html')
+    elif template_name == 'Aventura':
+        return render_template('Aventura.html')
+    elif template_name == 'Shonen':
+        return render_template('Shonen.html')
+    elif template_name == 'Jidaimono':
+        return render_template('Jidaimono.html')
+    elif template_name == 'Aventura':
+        return render_template('Aventura.html')
+    elif template_name == 'Accion':
+        return render_template('Accion.html')
+    elif template_name == 'Cap1':
+        return render_template('Cap1.html')
+
+    return render_template('Explorar.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
